f5ce75aa5547c269d37587046b9f85b7141f7905
Jasha Puzon <jpuzon@ucsc.edu>
2022-04-19 01:51:55 -0700

    comments

--- RocketPatrol1-main/src/main.js
+++ RocketPatrol1-main/src/main.js
@@ -1,3 +1,8 @@
+// Name: Jasha Puzon
+// Date: 4/19/2022
+// Project Title: CRUSTACEAN CONTROL
+// Hours Worked (thus far): 8
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
@@ -13,3 +18,10 @@ let keyUP, keyR, keyLEFT, keyRIGHT, keyA, keyD, keyW;
 // setting UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// Changes made:
+//      - simultaneous two player (30)
+//      - redesign game artwork, UI, and sound to change aesthetic (60)  
+//      
+//      
+//      more to be added...
